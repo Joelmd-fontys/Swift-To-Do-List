6a23c4c995fcc4a7602faf19b2a97b2d689b9c57
Joelmd-fontys <530004@student.fontys.nl>
2024-06-14 19:32:03 +0200

    Initial Commit

---
+++ Swift To-Do List/Swift_To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_To_Do_ListApp.swift
+//  Swift To-Do List
+//
+//  Created by Joel Malagarriga Duarte on 14.06.24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
